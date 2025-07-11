aff77c3337b3dfca08c2544d798bd480d2c58749
Satyam Yadav <satyamok03@gmail.com>
2025-07-11 17:26:59 +0530

    # cool pretier

--- src/app/join/[workspaceId]/page.tsx
+++ src/app/join/[workspaceId]/page.tsx
@@ -22,13 +22,12 @@ const JoinWorkspaceIdPage = () => {
   const { data, isLoading } = useGetWorkspaceInfo({ id: workspaceId });
 
   const isMember = useMemo(
-  () =>
-    data?.role === "admin" ||
-    data?.role === "member" ||
-    data?.role === "lead",
-  [data?.role]
-);
-
+    () =>
+      data?.role === "admin" ||
+      data?.role === "member" ||
+      data?.role === "lead",
+    [data?.role],
+  );
 
   useEffect(() => {
     if (isMember) router.push(`/workspace/${workspaceId}`);
